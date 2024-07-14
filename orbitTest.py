3be624aded5f431299bfff8afb075ce6414d5503
= <=>
2024-07-15 00:01:41 +0200

    💩 ✨ add variables, add debug stuff

--- orbitTest.py
+++ orbitTest.py
@@ -32,28 +32,45 @@ run = True
 
 moving = False
 
+
+
 # Create angle variables for each planet
-angle_mercury = 3
-angle_venus = 7
-angle_earth = 0
-angle_mars = 0
-angle_jupiter = 0
-angle_saturn = 0
-angle_uranus = 0
-angle_neptune = 0
-
-# ...
+angle_mercury = 0
+angle_venus = 1
+angle_earth = 1.55
+angle_mars = 3
+angle_jupiter = 4
+angle_saturn = 5
+angle_uranus = 6
+angle_neptune = 7
 
 
-
-#declare position of the sun
+#position sun
 sun_x = 750
 sun_y = 450
 
-#declare center position for planets
-
 
 #planet radius 
+radius_mercury = 100
+radius_venus = 150
+radius_earth = 200
+radius_mars = 250
+radius_jupiter = 300
+radius_saturn = 350
+radius_uranus = 400
+radius_neptune = 450
+
+
+#planes scale
+scale_sun = 100
+scale_mercury = 30
+scale_venus = 30
+scale_earth = 30
+scale_mars = 30
+scale_jupiter = 30
+scale_saturn = 30
+scale_uranus = 30
+scale_neptune = 30
 
 
 
@@ -68,84 +85,108 @@ while run:
 	- +[value]: coordinates - center of orbit
 		=> coordintes of top left corner of the image
 		=> center of the image is at (x + x-scale| y + y-scale)
-		=> center of the orbit is at (750 + (sun)x-scale/2 | 450 + (sun)y-scale/2)
-		=> to aling image in the orbit and let it spin around the sun: (sun-coordinates + radius of orbit - image-scale/2)
+		=> centerOfOrbit = (sun[coord] + (sun)x-scale/2 | 450 + (sun)y-scale/2)
+		=> to aling image in the orbit and let it spin around the sun: (centerOfOrbit - image-scale/2)
 	- angle1: angle of rotation
 	
 	"""
 	#move mercury around sun
-	x = 150 
-	y = 150
-	mercury = pygame.transform.scale(planets[1], (30, 30))
-	x = int(x*math.cos(angle_mercury)+835) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_mercury: angle of rotation
-	y = int(y*math.sin(angle_mercury)+535) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_mercury: angle of rotation
+	x = radius_mercury
+	y = radius_mercury
+	mercury = pygame.transform.scale(planets[1], (scale_mercury , scale_mercury))
+	x_center = sun_x + scale_sun/2 - scale_mercury/2
+	y_center = sun_y + scale_sun/2 - scale_mercury/2
+	x = (x * math.cos(angle_mercury) + x_center) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_mercury: angle of rotation
+	y = (y * math.sin(angle_mercury) + y_center) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_mercury: angle of rotation
 	window.fill((0, 0, 0))
 	window.blit(mercury, (x, y))
  
-	x = 150 
-	y = 150
-	venus = pygame.transform.scale(planets[2], (30, 30))
-	x = int(x*math.cos(angle_venus)+835) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_venus: angle of rotation
-	y = int(y*math.sin(angle_venus)+535) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_venus: angle of rotation
+	x = radius_venus
+	y = radius_venus
+	venus = pygame.transform.scale(planets[2], (scale_venus , scale_venus))
+	x_center = sun_x + scale_sun/2 - scale_venus/2
+	y_center = sun_y + scale_sun/2 - scale_venus/2
+	x = (x * math.cos(angle_venus) + x_center) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_venus: angle of rotation
+	y = (y * math.sin(angle_venus) + y_center) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_venus: angle of rotation
 	window.blit(venus, (x, y))
-	x=750
-	x = 200 
-	y = 200
-	earth = pygame.transform.scale(planets[3], (30, 30))
-	x = int(x*math.cos(angle_earth)+835) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_earth: angle of rotation
-	y = int(y*math.sin(angle_earth)+535) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_earth: angle of rotation
+
+	x = radius_earth
+	y = radius_earth
+	earth = pygame.transform.scale(planets[3], (scale_earth , scale_earth))
+	x_center = sun_x + scale_sun/2 - scale_earth/2
+	y_center = sun_y + scale_sun/2 - scale_earth/2
+	x = (x * math.cos(angle_earth) + x_center) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_earth: angle of rotation
+	y = (y * math.sin(angle_earth) + y_center) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_earth: angle of rotation
 	window.blit(earth, (x, y))
 
-	x = 250 
-	y = 250
-	mars = pygame.transform.scale(planets[4], (30, 30))
-	x = int(x*math.cos(angle_mars)+835) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_mars: angle of rotation
-	y = int(y*math.sin(angle_mars)+535) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_mars: angle of rotation
+	x = radius_mars
+	y = radius_mars
+	mars = pygame.transform.scale(planets[4], (scale_mars , scale_mars))
+	x_center = sun_x + scale_sun/2 - scale_mars/2
+	y_center = sun_y + scale_sun/2 - scale_mars/2
+	x = (x * math.cos(angle_mars) + x_center) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_mars: angle of rotation
+	y = (y * math.sin(angle_mars) + y_center) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_mars: angle of rotation
 	window.blit(mars, (x, y))
 
-	x = 300 
-	y = 300
-	jupiter = pygame.transform.scale(planets[5], (30, 30))
-	x = int(x*math.cos(angle_jupiter)+835) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_jupiter: angle of rotation
-	y = int(y*math.sin(angle_jupiter)+535) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_jupiter: angle of rotation
+	x = radius_jupiter
+	y = radius_jupiter
+	jupiter = pygame.transform.scale(planets[5], (scale_jupiter , scale_jupiter))
+	x_center = sun_x + scale_sun/2 - scale_jupiter/2
+	y_center = sun_y + scale_sun/2 - scale_jupiter/2
+	x = (x * math.cos(angle_jupiter) + x_center) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_jupiter: angle of rotation
+	y = (y * math.sin(angle_jupiter) + y_center) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_jupiter: angle of rotation
 	window.blit(jupiter, (x, y))
 
-	x = 350 
-	y = 350
-	saturn = pygame.transform.scale(planets[6], (30, 30))
-	x = int(x*math.cos(angle_saturn)+835) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_saturn: angle of rotation
-	y = int(y*math.sin(angle_saturn)+535) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_saturn: angle of rotation
+	x = radius_saturn
+	y = radius_saturn
+	saturn = pygame.transform.scale(planets[6], (scale_saturn , scale_saturn))
+	x_center = sun_x + scale_sun/2 - scale_saturn/2
+	y_center = sun_y + scale_sun/2 - scale_saturn/2
+	x = (x * math.cos(angle_saturn) + x_center) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_saturn: angle of rotation
+	y = (y * math.sin(angle_saturn) + y_center) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_saturn: angle of rotation
 	window.blit(saturn, (x, y))
 
-	x = 400 
-	y = 400
-	uranus = pygame.transform.scale(planets[7], (30, 30))
-	x = int(x*math.cos(angle_uranus)+835) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_uranus: angle of rotation
-	y = int(y*math.sin(angle_uranus)+535) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_uranus: angle of rotation
+	x = radius_uranus
+	y = radius_uranus
+	uranus = pygame.transform.scale(planets[7], (scale_uranus , scale_uranus))
+	x_center = sun_x + scale_sun/2 - scale_uranus/2
+	y_center = sun_y + scale_sun/2 - scale_uranus/2
+	x = (x * math.cos(angle_uranus) + x_center) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_uranus: angle of rotation
+	y = (y * math.sin(angle_uranus) + y_center) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_uranus: angle of rotation
 	window.blit(uranus, (x, y))
 
-	x = 450 
-	y = 450
-	neptune = pygame.transform.scale(planets[8], (30, 30))
-	x = int(x*math.cos(angle_neptune)+835) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_neptune: angle of rotation
-	y = int(y*math.sin(angle_neptune)+535) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_neptune: angle of rotation
+	x = radius_neptune
+	y = radius_neptune
+	neptune = pygame.transform.scale(planets[8], (scale_neptune , scale_neptune))
+	x_center = sun_x + scale_sun/2 - scale_neptune/2
+	y_center = sun_y + scale_sun/2 - scale_neptune/2
+	x = (x * math.cos(angle_neptune) + x_center) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_neptune: angle of rotation
+	y = (y * math.sin(angle_neptune) + y_center) #x/y: radius of orbit, +[value]: coordinates - center of orbit, angle_neptune: angle of rotation
 	window.blit(neptune, (x, y))
  
 	x = sun_x
 	y = sun_y
-	sun = pygame.transform.scale(planets[0] , (200, 200))
+	sun = pygame.transform.scale(planets[0] , (scale_sun , scale_sun))
 	window.blit(sun, (x, y))
+	
+	sun = pygame.transform.scale(planets[0] , (2 , 1500))
+	window.blit(sun, (x + scale_sun/2, y + scale_sun/2))
+	sun = pygame.transform.scale(planets[0] , (2 , 1500))
+	window.blit(sun, (x + scale_sun/2, y + scale_sun/2))
+	
+ 
 	sleep(0.1)
+ 
 
 		# Update the angle variables for each planet
-	angle_mercury += 0.1
-	angle_venus += 0.1
-	angle_earth += 0.1
-	angle_mars += 0.1
-	angle_jupiter += 0.1
-	angle_saturn += 0.1
-	angle_uranus += 0.1
-	angle_neptune += 0.1
+	# angle_mercury += 0.1
+	# angle_venus += 0.1
+	# angle_earth += 0.1
+	# angle_mars += 0.1
+	# angle_jupiter += 0.1
+	# angle_saturn += 0.1
+	# angle_uranus += 0.1
+	# angle_neptune += 0.1
 
 	# ...
  
@@ -155,5 +196,4 @@ while run:
 			run = False
 			pygame.quit()
 			quit()
-
 	pygame.display.update()
