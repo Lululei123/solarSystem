4f054af25c920b39a1f8129f3d01c2df8e412bf4
Matthias Fuetterer <mazzefm@gmail.com>
2024-07-15 02:53:53 +0200

    ✨ make project more realistic/ proportional

--- orbitTest.py
+++ orbitTest.py
@@ -32,44 +32,46 @@ run = True
 moving = False
 
 
+degree_preset = math.pi/180
+dist_around_sun = math.pi*2
+speed_multiplier = 1
 
 # Create angle variables for each planet
-angle_mercury = 0
-angle_venus = 1
-angle_earth = 1.55
-angle_mars = 3
-angle_jupiter = 4
-angle_saturn = 5
-angle_uranus = 6
-angle_neptune = 7
+angle_mercury = degree_preset * 180		# CONFIG angle: 300
+angle_venus = degree_preset * 270		# CONFIG angle: 270
+angle_earth = degree_preset * 105		# CONFIG angle: 105
+angle_mars = degree_preset * 345		# CONFIG angle: 345
+angle_jupiter = degree_preset * 210		# CONFIG angle: 210
+angle_saturn = degree_preset * 30		# CONFIG angle: 30
+angle_uranus = degree_preset * 195		# CONFIG angle: 195
+angle_neptune = degree_preset * 240 	# CONFIG angle: 240
 
 
 #position sun
-sun_x = 750
-sun_y = 450
+sun_x = 900
+sun_y = 470
 
 
 #planet radius 
-radius_mercury = 100
-radius_venus = 150
-radius_earth = 200
-radius_mars = 250
-radius_jupiter = 300
-radius_saturn = 350
-radius_uranus = 400
-radius_neptune = 450
-
+radius_mercury = 200
+radius_venus = 250 
+radius_earth = 300 
+radius_mars = 350 
+radius_jupiter = 400 
+radius_saturn = 450 
+radius_uranus = 500 
+radius_neptune = 550 
 
 #planes scale
-scale_sun = 100
-scale_mercury = 30
-scale_venus = 30
-scale_earth = 30
-scale_mars = 30
-scale_jupiter = 30
-scale_saturn = 30
-scale_uranus = 30
-scale_neptune = 30
+scale_sun = 280 # Real diameter: 1'392'000 km
+scale_mercury = 4 # Real diameter: 4'879 km
+scale_venus = 12 # Real diameter: 12'104 km
+scale_earth = 12 # Real diameter: 12'756 km
+scale_mars = 6 # Real diameter: 6'792 km
+scale_jupiter = 139 # Real diameter: 139'820 km
+scale_saturn = 116 # Real diameter: 116'460 km
+scale_uranus = 50 # Real diameter: 50'724 km
+scale_neptune = 49 # Real diameter: 49'244 km
 
 
 
@@ -178,14 +180,14 @@ while run:
  
 
 		# Update the angle variables for each planet
-	# angle_mercury += 0.1
-	# angle_venus += 0.1
-	# angle_earth += 0.1
-	# angle_mars += 0.1
-	# angle_jupiter += 0.1
-	# angle_saturn += 0.1
-	# angle_uranus += 0.1
-	# angle_neptune += 0.1
+	angle_mercury += dist_around_sun /88 * speed_multiplier		#is the speed of a surrounding time of 88 days
+	angle_venus += dist_around_sun /225 * speed_multiplier	#is the speed of a surrounding time of 225 days
+	angle_earth += dist_around_sun /365 * speed_multiplier		#is the speed of a surrounding time of 365 days
+	angle_mars += dist_around_sun /687 * speed_multiplier		#is the speed of a surrounding time of 687 days
+	angle_jupiter += dist_around_sun /4333 * speed_multiplier	#is the speed of a surrounding time of 4333 days
+	angle_saturn += dist_around_sun /10759 * speed_multiplier	#is the speed of a surrounding time of 10759 days
+	angle_uranus += dist_around_sun /30687 * speed_multiplier	#is the speed of a surrounding time of 30687 days
+	angle_neptune += dist_around_sun /60190 * speed_multiplier	#is the speed of a surrounding time of 60190 days
 
 	# ...
  
